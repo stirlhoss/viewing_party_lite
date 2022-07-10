7ac1582ae8eaeae6cef178889ffb3dfe1f7f08c1
Stirling Hostetter <stirhoss@icloud.com>
2022-07-10 17:05:35 -0500

    Remove need for attendees

--- spec/features/user/dashboard_spec.rb
+++ spec/features/user/dashboard_spec.rb
@@ -32,21 +32,18 @@ RSpec.describe 'user dashboard' do
     user9 = User.create!(name: 'Stirling', email: 'stirbydirbydoo@gmail.com')
     party1 = ViewingParty.create!(movie_title: 'Hot Rod',
                                   duration: 90,
-                                  # attendees: [user2, user3, user4, user1],
                                   start_date: '2022-08-19',
                                   start_time: '18:00',
                                   image: 'hot_rod_image',
                                   host_id: user2.id)
     party2 = ViewingParty.create!(movie_title: 'Semipro',
                                   duration: 90,
-                                  # attendees: [user5, user6, user7],
                                   start_date: '2022-08-19',
                                   start_time: '17:00',
                                   image: 'semipro_image',
                                   host_id: user2.id)
     party3 = ViewingParty.create!(movie_title: 'Barry Lyndon',
                                   duration: 180,
-                                  # attendees: [user8, user9, user1],
                                   start_date: '2022-08-19',
                                   start_time: '12:00',
                                   image: 'semipro_image',
@@ -71,11 +68,6 @@ RSpec.describe 'user dashboard' do
       expect(page).to have_content('Barry Lyndon')
       expect(page).to_not have_content('Semipro')
     end
-    # must include: movie image,
-    #               movie title(as link to movie show page),
-    #               date and time of event,
-    #               who is hosting the event,
-    #               list of users invited
   end
 
   it 'show a list of viewing parties the user has created' do
@@ -129,10 +121,5 @@ RSpec.describe 'user dashboard' do
       expect(page).to_not have_content('Semipro')
       expect(page).to_not have_content('Hot Rod')
     end
-    # must include: movie image,
-    #               movie title(as link to movie show page),
-    #               Date and time of event,
-    #               That I am the host of the party,
-    #               List of friends invited to the viewing party
   end
 end
